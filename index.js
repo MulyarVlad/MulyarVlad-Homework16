0e722ac8419b65f194e6c6e1391a3d9b22f1ba75
Mulyar Vlad <mulyarvlad@MulyarVlad.local>
2023-02-21 22:49:48 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+function generateKey(length, characters) {
+    let result = '';
+    for(let i = 0; i <= length; i++){
+        result+=characters.charAt(Math.floor(Math.random()*characters.length));
+    }
+    return result;
+};
+
+console.log(generateKey(16, characters))
